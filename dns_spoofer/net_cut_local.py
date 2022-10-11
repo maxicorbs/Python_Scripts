3886553a89b607a7ee71475cc4a1902fad6fa8cf
maxicorbs <61736384+maxicorbs@users.noreply.github.com>
2022-10-11 11:32:38 +0100

    day 16

--- dns_spoofer/net_cut_local.py
+++ dns_spoofer/net_cut_local.py
@@ -2,19 +2,21 @@
 import subprocess
 import netfilterqueue
 import scapy.all as scapy
+from scapy.utils import hexdump
 
 def process_packet(packet):
-    scapy_packet = scapy.IP(packet.getpayload())
-    print(scapy.packet.show())
+    scapy_packet = scapy.IP(packet.get_payload())
+    print(scapy_packet.show())
+    hexdump(packet)
     packet.accept()
 
 
 try:
     print("[+] Amening IP tables")
-    subprocess.call("iptables -I INPUT -j NFQUEUE --queue-num 0", shell=True)
-    subprocess.call("iptables -I OUTPUT -j NFQUEUE --queue-num 0", shell=True)
+    subprocess.call("iptables -I INPUT -j NFQUEUE --queue-num 1", shell=True)
+    subprocess.call("iptables -I OUTPUT -j NFQUEUE --queue-num 1", shell=True)
     queue = netfilterqueue.NetfilterQueue()
-    queue.bind(0, process_packet)
+    queue.bind(1, process_packet)
     queue.run()
 
 except KeyboardInterrupt:
